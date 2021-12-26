b796740d7234afa42956b5642a8a33e7eff21eb5
Onur Kacmaz <kacmaz.onur@hotmail.com>
2021-12-26 23:20:36 +0300

    add migration.go and sql of tables

---
+++ database/migration/sql/tasks.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tasks (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    user_id int NOT NULL,
+    title varchar(255) UNIQUE NOT NULL,
+    content LONGTEXT NOT NULL,
+    status ENUM('completed', 'uncompleted'),
+    created_at TIMESTAMP NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
---
+++ database/migration/sql/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL ,
+    password varchar(255) NOT NULL,
+    created_at TIMESTAMP NULL,
+    PRIMARY KEY (id)
+)
