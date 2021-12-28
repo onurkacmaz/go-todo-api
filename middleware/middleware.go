61fa5308e566a4baf898833a20cbe67916c568a3
Onur Kacmaz <kacmaz.onur@hotmail.com>
2021-12-29 00:47:47 +0300

    add basic auth middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,8 +1,12 @@
 package middleware
 
 import (
+	b64 "encoding/base64"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"rest-api/controller"
+	"strings"
 )
 
 type Response struct {
@@ -14,6 +18,7 @@ func ContentType(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		c := r.Header.Get("Content-Type")
 		if c != "application/json" {
+			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(Response{
 				Status:  400,
 				Message: "Content-Type parameter is required and must be application/json.",
@@ -28,9 +33,34 @@ func Accept(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		c := r.Header.Get("Accept")
 		if c != "application/json" {
+			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(Response{
 				Status:  400,
 				Message: "Accept parameter is required and must be application/json.",
+			})
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func BasicAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		base64 := strings.Replace(auth, "Basic ", "", -1)
+		fmt.Println(base64)
+		data, err := b64.StdEncoding.DecodeString(base64)
+		if err != nil {
+			panic(err)
+		}
+		credentials := strings.Split(string(data), ":")
+		email := credentials[0]
+		password := credentials[1]
+		if !controller.Check(email, password) {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(Response{
+				Status:  401,
+				Message: "Invalid credentials",
 			})
 			return
 		}
