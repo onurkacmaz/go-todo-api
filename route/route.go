266cfc5cbf401b3056cf444440d8b72cdc600252
Onur Kacmaz <kacmaz.onur@hotmail.com>
2021-12-28 22:10:10 +0300

    rename controllers to controller

--- route/route.go
+++ route/route.go
@@ -2,7 +2,7 @@ package route
 
 import (
 	"github.com/gorilla/mux"
-	"rest-api/controllers"
+	"rest-api/controller"
 )
 
 var router = route()
@@ -13,16 +13,16 @@ func route() *mux.Router {
 
 func RegisterRoutes() *mux.Router {
 	usersRouter := router.PathPrefix("/api/v1").Subrouter()
-	usersRouter.HandleFunc("/users", controllers.CreateUser).Methods("POST")
-	usersRouter.HandleFunc("/users", controllers.GetUsers).Methods("GET")
-	usersRouter.HandleFunc("/users/{id}", controllers.ShowUser).Methods("GET")
-	usersRouter.HandleFunc("/users/{id}", controllers.DeleteUser).Methods("DELETE")
-	usersRouter.HandleFunc("/users/{id}", controllers.UpdateUser).Methods("PUT")
+	usersRouter.HandleFunc("/users", controller.CreateUser).Methods("POST")
+	usersRouter.HandleFunc("/users", controller.GetUsers).Methods("GET")
+	usersRouter.HandleFunc("/users/{id}", controller.ShowUser).Methods("GET")
+	usersRouter.HandleFunc("/users/{id}", controller.DeleteUser).Methods("DELETE")
+	usersRouter.HandleFunc("/users/{id}", controller.UpdateUser).Methods("PUT")
 
-	usersRouter.HandleFunc("/tasks", controllers.CreateTask).Methods("POST")
-	usersRouter.HandleFunc("/tasks", controllers.GetTasks).Methods("GET")
-	usersRouter.HandleFunc("/tasks/{id}", controllers.ShowTask).Methods("GET")
-	usersRouter.HandleFunc("/tasks/{id}", controllers.DeleteTask).Methods("DELETE")
-	usersRouter.HandleFunc("/tasks/{id}", controllers.UpdateTask).Methods("PUT")
+	usersRouter.HandleFunc("/tasks", controller.CreateTask).Methods("POST")
+	usersRouter.HandleFunc("/tasks", controller.GetTasks).Methods("GET")
+	usersRouter.HandleFunc("/tasks/{id}", controller.ShowTask).Methods("GET")
+	usersRouter.HandleFunc("/tasks/{id}", controller.DeleteTask).Methods("DELETE")
+	usersRouter.HandleFunc("/tasks/{id}", controller.UpdateTask).Methods("PUT")
 	return router
 }
